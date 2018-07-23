06af97bd243c84047328b55accda2b500a9f57ab
Cristian Acuña S <c.acuna.sepulveda@gmail.com>
2018-07-23 06:11:55 -0400

    Scrapper and Modeling scrippts updated

---
+++ code/ModelTesting.R
@@ -0,0 +1,144 @@
+
+# Clean and set up environment --------------------------------------------
+
+rm(list = ls())
+
+setwd("Z:/Cristian Acuna/Module 2/Research Project/Toy to Enjoy/code")
+
+library(magrittr)
+library(readr)
+library(tm)
+library(RColorBrewer)
+library(caret)
+library(e1071)
+library(ranger)
+library(SnowballC)
+library(wordcloud)
+library(ggplot2)
+library(dplyr)
+library(tictoc)
+#library(kknn)
+
+# Load data ---------------------------------------------------------------
+
+reviews <- read_csv("../input/reviews.csv")
+reviews <- reviews[, -1]
+reviews <- reviews[!(is.na(reviews$Review) | reviews$Review == ""), ]
+reviews$Review %<>%  iconv("ASCII//TRANSLIT")
+reviews$Review %<>% tolower()
+reviews[reviews$Review %in% c("100%", "100 %"), "Review"] <- "excellent"
+
+# Subseting dataset -------------------------------------------------------
+
+fiveStars	<- reviews[reviews$Rating == 5, ]
+fourStars 	<- reviews[reviews$Rating == 4, ]
+threeStars 	<- reviews[reviews$Rating == 3, ]
+twoStars 	<- reviews[reviews$Rating == 2, ]
+oneStar  	<- reviews[reviews$Rating == 1, ]
+
+fiveStars 	%<>%  sample_n(size = 200)
+fourStars 	%<>%  sample_n(size = 200)
+threeStars 	%<>%  sample_n(size = 200)
+twoStars 	%<>%  sample_n(size = 200)
+oneStar 	%<>%  sample_n(size = 200)
+
+reviews <- rbind(fiveStars, fourStars, threeStars, twoStars, oneStar)
+
+# Building the corpus -----------------------------------------------------
+
+reviews_comments <- reviews$Review
+reviews_com_source <- VectorSource(reviews_comments)
+reviews_com_corpus <- VCorpus(reviews_com_source)
+#str(reviews_com_corpus)
+
+clean_corpus <- function(corpus){
+	corpus <- tm_map(corpus, removePunctuation)
+	corpus <- tm_map(corpus, removeNumbers)
+	corpus <- tm_map(corpus, removeWords, c(stopwords("en"), "Patatas"))
+	corpus <- tm_map(corpus, stripWhitespace)
+	return(corpus)
+}
+
+clean_reviews <- clean_corpus(reviews_com_corpus)
+
+# Building DTM ------------------------------------------------------------
+
+reviews_dtm <- 
+	DocumentTermMatrix(
+		clean_reviews, 
+		list(
+			removePunctuation = TRUE,
+			removeNumbers = TRUE,
+			stopwords = TRUE, 
+			weighting = weightTfIdf))
+
+reviews_matrix <- as.matrix(reviews_dtm)
+reviews_dt <- as.data.frame(reviews_matrix)
+
+dataset <- cbind(reviews_dt, "Score" = as.factor(reviews$Rating))
+
+# building the test and training sets -------------------------------------
+
+train_index <- createDataPartition(y = dataset$Score, p = 0.75, list = FALSE)
+train <- dataset[train_index, ]
+test <- dataset[-train_index, ]
+
+# training the models -----------------------------------------------------
+
+#first_train_SVM <- train(Score~.,data = train, method = "svmLinear2")
+#first_predict_SVM <- predict(first_train_SVM,test)
+#confusionMatrix(first_predict_SVM,test$Score)
+
+#first_train_RandForest <- train(Score~.,data = train, method = "ranger")
+#first_predict_randForest <- predict(first_train_RandForest,test)
+#confusionMatrix(first_predict_randForest ,test$Score)
+
+#svm_model <- svm(reviews_dt, train$Score, cost = 100, gamma = 1)
+
+# svm linear --------------------------------------------------------------
+
+tic("SVM Linear")
+
+svm_model <- svm(train[, 1:(ncol(train) - 1)], train$Score, cost = 100, kernel = "linear")
+svm_pred <- predict(svm_model, test[,-ncol(test)])
+confusionMatrix(svm_pred, test$Score)
+
+toc()
+# tgrid <- expand.grid(
+# 	.num.trees = 50
+# )
+
+# ranger num.trees=50 -----------------------------------------------------
+
+tic("RandomForest with num.trees=50")
+
+ranger_model <- train(x = train[, -ncol(train)], 
+					  y = train$Score,
+					  method = "ranger",
+					  num.trees = 50)
+ranger_pred <- predict(ranger_model, test)
+confusionMatrix(ranger_pred ,test$Score)
+
+toc()
+
+# svm rbf -----------------------------------------------------------------
+
+tic("SVM Radial Basis Function")
+
+svm_rbf_model <- svm(train[, 1:(ncol(train) - 1)], train$Score, cost = 100, gamma = 1)
+svm_rbf_pred <- predict(svm_rbf_model, test[,-ncol(test)])
+confusionMatrix(svm_rbf_pred, test$Score)
+
+toc()
+
+# kNN ---------------------------------------------------------------------
+
+tic("kNN")
+
+knn_model <- train(x = train[, -ncol(train)], 
+				   y = train$Score,
+				   method = "knn")
+knn_pred <- predict(knn_model, test[,-ncol(test)])
+confusionMatrix(knn_pred, test$Score)
+
+toc()
---
+++ code/scrapper.R
@@ -0,0 +1,109 @@
+# Clean and setup ---------------------------------------------------------
+
+rm(list = ls())
+
+library(rvest)
+library(tidyverse)
+library(stringr)
+library(rebus)
+library(lubridate)
+
+# Function definitions ----------------------------------------------------
+
+# Function get_last_page
+get_last_page <- function(html){
+	pages_data <- html %>% html_nodes(".page-button")%>% html_text()
+	pages_data[(length(pages_data))] %>% unname() %>% as.numeric()
+}
+
+Scrap <- function(urlAddr){
+	# Acquiring html page
+	scrapped <- "../output/scrapedpage.html"
+	download.file(urlAddr, destfile = scrapped, quiet = TRUE)
+	webpage <- read_html(scrapped)
+
+	# Extracting ratings
+	rank_data_html <- html_nodes(webpage, ".a-icon-alt") %>% html_text()
+	rank_data_html <- rank_data_html[rank_data_html != "|" ]
+	only_stars <- str_sub(rank_data_html,1,1)
+	new_rating <- c(only_stars[4:13])
+
+	# Extracting comments
+
+	Comment <- html_nodes(webpage,".review-text , .a-spacing-top-mini .a-size-base") %>% html_text()
+	new_comments <- c(Comment[3:13])
+	pos <- complete.cases(new_comments)
+	new_comments <- new_comments[pos]
+
+	Reviews <- cbind(Rating = new_rating, Review = new_comments) %>% as_tibble()
+	return (Reviews)
+}
+
+
+scrappe <- function(baseURL) {
+	download.file(baseURL, destfile = "scrapedpage.html", quiet = TRUE)
+
+	# Get the last page
+	scrapped <- "../output/scrapedpage.html"
+	webpage <- read_html(scrapped)
+
+	latest_page_number <- get_last_page(webpage)
+
+	list_of_pages <- str_c(baseURL, "&pageNumber=", 1:latest_page_number)
+
+	answer <- tibble()
+
+	for (i in list_of_pages){
+		answer %<>% rbind(Scrap(i))
+	}
+
+	return(answer)
+}
+
+# JUGUETE 1 Crayons -------------------------------------------------------
+
+direccion <- "https://www.amazon.com/ALEX-Toys-Rub-Draw-Crayons/product-reviews/B000G2FOLC/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
+
+answer1 <- scrappe(direccion)
+
+answer1 <- answer1[1:431,]
+
+answer1 %>% View
+
+# JUGUETE 2 Boon Building Bath Pipes Toy ----------------------------------
+
+direccion <- "https://www.amazon.com/Boon-Building-Bath-Pipes-Toy/product-reviews/B00R0V7PUU/ref=cm_cr_dp_d_show_all_top?ie=UTF8&reviewerType=all_reviews"
+
+answer2 <- scrappe(direccion)
+
+answer2 <- answer2[1:1219,]
+
+# JUGUETE 3 NICK JR PAW ---------------------------------------------------
+
+direccion <- "https://www.amazon.com/Nick-Paw-Patrol-Bifold-Wallet/product-reviews/B01836F31I/ref=cm_cr_othr_d_paging_btm_1?ie=UTF8&reviewerType=all_reviews"
+
+answer3 <- scrappe(direccion)
+
+answer3 <- answer3[1:260,]
+
+# JUGUETE 4 LIGHT-UP TOY WATERPROOF ---------------------------------------
+
+direccion <- "https://www.amazon.com/Light-up-Waterproof-Floating-Instruction-Educational/product-reviews/B078Y98FSB/ref=cm_cr_dp_d_show_all_top?ie=UTF8&reviewerType=all_reviews"
+
+answer4 <- scrappe(direccion)
+
+answer4 <- answer4[1:160,]
+
+# JUGUETE 5 MOTOROLA MBP854 -----------------------------------------------
+
+direccion <- "https://www.amazon.com/Motorola-MBP854CONNECT-Monitor-4-3-Inch-Internet/product-reviews/B00ROEBAK4/ref=cm_cr_dp_d_show_all_top?ie=UTF8&reviewerType=all_reviews"
+
+answer5 <- scrappe(direccion)
+
+answer5 <- answer5[1:1025,]
+
+Reviews_All <- rbind(answer1, answer2, answer3, answer4, answer5)
+
+# save scrapping ----------------------------------------------------------
+
+write.csv(Reviews_All, file = "../output/scrapped_reviews.csv")
